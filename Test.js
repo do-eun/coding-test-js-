26650bd532e6370de40dda2f9b48e5862b45c9ae
doeun <drakequation@naver.com>
2022-12-27 23:48:03 +0900

    [LV0] 문제풀이 12/27

---
+++ Test.js
@@ -0,0 +1,34 @@
+function solution(age) {
+  let word = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  return String(age)
+    .split("")
+    .map((x) => word[Number(x)])
+    .join("");
+}
